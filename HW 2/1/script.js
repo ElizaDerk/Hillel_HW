5117b2091ccdc8628ffe8df24b12aedbf31dc137
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-06-15 20:31:43 +0300

    Second HomeWork part1

---
+++ HW 2/1/script.js
@@ -0,0 +1,10 @@
+let userName = prompt('Enter you name!');
+let userPassword = prompt('Enter your password')
+    if (userName === 'admin' && userPassword === 'qWeRty123'){
+        alert('Entrance allowed!')
+    }
+    else {
+        alert('No entry!')
+    }
+
+
