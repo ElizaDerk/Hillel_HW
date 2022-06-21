04c37712cffb2192903646ee28941b461d5fb699
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-06-21 14:39:19 +0300

    Second HomeWork

---
+++ HW 2/3/script.js
@@ -0,0 +1,12 @@
+let a = prompt("Enter the first number?");
+let b = prompt("Enter the second number?");
+let c = prompt("Enter the third number?");
+let d = prompt("Enter the fourth number?");
+
+
+alert(Math.min( a, b, c, d ) * Math.max( a, b, c, d ))
+
+
+
+
+
