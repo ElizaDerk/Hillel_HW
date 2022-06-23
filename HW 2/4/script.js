fd13a0b96c8bcd3efa2439cc577683b8cfdea8fd
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-06-23 11:05:18 +0300

    Second HomeWork

--- HW 2/4/script.js
+++ HW 2/4/script.js
@@ -1,7 +1,36 @@
-let a = prompt('Enter position', 'x.y')
-let b = prompt('Enter position', 'x.y')
+let place = alert('Your first place')
+    let a = prompt('Enter x', 1)
+    let b = prompt('Enter y', 1)
 
+if (a>=1 && a<=8 && b>=1 && b<=8) {
+    alert(true);
 
+    let step = alert('Please, do the step')
+        var x = prompt('Enter position for x', 1)
+        var y= prompt('Enter position for y', 1)
+    if (x>=1 && x<=8 && y>=1 && y<=8){
+        if ((a+x) && (b+y)){
+            if(x<=2 && y<=2) {
+                alert('True')
+            }
+                else if ( (a=x) && (b=y)){
+                    alert('False')
+                }
+        }
+
+        else if ((a-x) && (b-y)) {
+            if(x<=2 && y<=2){
+                alert('True')
+            }
+        }
+    }
+    else {
+        alert('False')
+    }
+}
+    else {
+    alert('False')
+}
 
 
 
