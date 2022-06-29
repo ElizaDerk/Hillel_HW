37c3d32d5391db5401007eeb47d7ac34cd6152be
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-06-29 19:39:10 +0300

    Exercize_1

---
+++ HW 3/1/script.js
@@ -0,0 +1,10 @@
+let startX = +prompt("Let's enter the number X");
+let endY = +prompt("Let's enter the number Y");
+    if(startX > 0 && endY > 0) {
+        for (startX = 0 ; startX <= endY; startX++ ) {
+            alert(startX);
+        }
+    }else{
+        alert("Try again!")
+    }
+
