a41dd08cf15409af008e3ed78ee2ef2d07958398
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-06-30 18:24:04 +0300

    correct Math.random

--- HW 3/2/script.js
+++ HW 3/2/script.js
@@ -1,16 +1,13 @@
-let arrLength = parseInt(prompt("Количество цифр, которые хотите ввести"));
-        if (arrLength > 0){
-            var arr = [];
-            let sum = 0;
-            for ( let newArr = 0; newArr < arrLength; newArr++) {
-               let number = parseInt(prompt("Введите число"));
-               arr.push(number);
-               sum += number;
-            }
-            let x = sum /arrLength;
-            alert("Array:" + arr.toString()+". Mean:"+ x.toString())
+let arrLength = parseInt(prompt("Количество цифр в массиве"));
+if (arrLength > 0){
+    var arr = [];
+    let sum = 0;
+    for ( let newArr = 0; newArr < arrLength; newArr++) {
+        let number = Math.trunc(Math.random()*999999) + 1;
+        arr.push(number);
+        sum += number;
+    }
+    let x = sum /arrLength;
+    alert("Array:" + arr.toString()+". Mean:"+ x.toString())
 
-        }
-
-
-
+}
