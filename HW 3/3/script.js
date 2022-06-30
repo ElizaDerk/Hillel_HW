934a4c6f7ff140ad90aa75e855dca0aa325e7fcc
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-06-30 20:51:23 +0300

    Ex_3

--- HW 3/3/script.js
+++ HW 3/3/script.js
@@ -1,12 +1,23 @@
 let arrLength = parseInt(prompt("Количество цифр, которые хотите ввести"));
 if (arrLength > 0){
-    var arr = [];
+    var arrFirst = [];
     for ( let newArr = 0; newArr < arrLength; newArr++) {
         let number = parseInt(prompt("Введите число"));
-        arr.push(number);
+        arrFirst.push(number);
     }
-    alert("Array:" + arr.toString())
+    alert("Array:" + arrFirst.toString())
 }
 
+
+let minArr = arrFirst.indexOf(Math.min(...arrFirst))
+let maxArr = arrFirst.indexOf(Math.max(...arrFirst))
+
+let minNumber = arrFirst[minArr]
+let maxNumber = arrFirst[maxArr]
+
+arrFirst[minArr] = maxNumber
+arrFirst[maxArr] = minNumber
+
+alert("Array:" + arrFirst.toString() + "." + " Минимальное значение:" + Math.min(...arrFirst) + "." + " Максимальное значение:" + Math.max(...arrFirst) + "." )
 
 
