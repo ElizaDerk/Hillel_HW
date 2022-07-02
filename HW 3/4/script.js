f1dd05067f9d97fb1cd72dce8ad1e7b9827d0512
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-07-02 10:55:22 +0300

    Ex_4

---
+++ HW 3/4/script.js
@@ -0,0 +1,18 @@
+
+
+
+//Pop
+let numbers = [1, 2, 3, 4]
+function pop(arr) {
+    let res = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        res[i] = arr[i]
+    }
+    return (res);
+}
+c = pop(numbers)
+
+
+
+
+
