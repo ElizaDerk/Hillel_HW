c481476a6b9f1f1e5e669d61a5b6c1b485f4a143
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-07-03 16:37:02 +0300

    Ex_5

--- HW 3/1/script.js
+++ HW 3/1/script.js
@@ -1,10 +1,6 @@
 let startX = +prompt("Let's enter the number X");
 let endY = +prompt("Let's enter the number Y");
-    if(startX > 0 && endY > 0) {
-        for (startX = 0 ; startX <= endY; startX++ ) {
+        for (; startX <= endY; startX++ ) {
             alert(startX);
         }
-    }else{
-        alert("Try again!")
-    }
 
