3b2849f3106a98985d40cfcca98ea0a50e925772
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-07-13 14:40:32 +0300

    1 ex: MAX

---
+++ HW 5/2/script.js
@@ -0,0 +1,11 @@
+let n = parseInt(prompt('Enter the number N'))
+let m = parseInt(prompt('Enter the number M'))
+
+function pow(n,m){
+    let result = 1;
+    for (let i = 0; i < m; i++) {
+        result *= n;
+    }
+    return result;
+}
+console.log(pow(n,m))
