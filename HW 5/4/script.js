158a33f4acdf1d460572ccd990084e56a50780cf
Eliz <98046082+ElizaDerk@users.noreply.github.com>
2022-07-14 18:19:52 +0300

    4 ex: final version

--- HW 5/4/script.js
+++ HW 5/4/script.js
@@ -2,7 +2,8 @@ function doFunction(x, y, foo){
     return foo(x,y)
 }
 function quotient(x, y){
-    return x / y
+    return parseInt(x / y)
 }
 
 console.log(doFunction(5, 2, quotient))
+console.log(doFunction(7, 2, quotient))
